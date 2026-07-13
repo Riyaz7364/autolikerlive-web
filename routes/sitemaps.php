6e5482a87a91759be8dac93af392e2884d154a08
riyaz7364 <rseifi.a73@gmail.com>
2026-07-13 12:37:15 +0530

    xml path updated

--- routes/sitemaps.php
+++ routes/sitemaps.php
@@ -4,25 +4,25 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('sitemap-generator', [SitemapController::class, 'createSitemap']);
-// Route::get('sitemap.xml', function(){
-//     return response()->file(public_path('sitemap.xml'), [
+Route::get('sitemap.xml', function(){
+    return response()->file(public_path('sitemap.xml'), [
+        'Content-Type' => 'application/xml',
+        'Accept' => 'application/xml',
+    ]);
+});
+// Route::get('sitemap-en.xml', function(){
+//     return response()->file(public_path('sitemap-en.xml'), [
 //         'Content-Type' => 'application/xml',
 //         'Accept' => 'application/xml',
 //     ]);
 // });
-Route::get('sitemap-en.xml', function(){
-    return response()->file(public_path('sitemap-en.xml'), [
-        'Content-Type' => 'application/xml',
-        'Accept' => 'application/xml',
-    ]);
-});
 
-Route::get('sitemap-bn.xml', function(){
-    return response()->file(public_path('sitemap-bn.xml'), [
-        'Content-Type' => 'application/xml',
-        'Accept' => 'application/xml',
-    ]);
-});
+// Route::get('sitemap-bn.xml', function(){
+//     return response()->file(public_path('sitemap-bn.xml'), [
+//         'Content-Type' => 'application/xml',
+//         'Accept' => 'application/xml',
+//     ]);
+// });
 
 Route::get('robots.txt', function(){
     return response()->file(public_path('robots.txt'), [
